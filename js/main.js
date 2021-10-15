5f9b2b1cd8a08f14a7e58a52ccef09277c0246c8
Nata Pabon <ta@MacBook-Air-de-Ta.local>
2021-10-14 22:48:12 -0400

    ading the data and functions

--- js/main.js
+++ js/main.js
@@ -5,22 +5,25 @@
     // set up a Fetch function and get some data
     function getData() {
         // retrieve our data object
+        // use the fetch API to retrive some data
         fetch("./data.json") // go and get the data (fetch boy!)
-            .then(res => res.json()) // good dog! clean the stick (convert the data to a plain object)
+            .then(res => res.json()) // good dog! clean the stick (convert the data to a plain JS object)
             .then(data => {
                 console.log(data);
+                // do anything else here that we want with our data
+                //call a function to generate our dynamic content
 
                 buildTeam(data);
             })
-        .catch(error => console.error(error));
+        .catch(error => console.error(error)); // catch and report any errors
     }
 
     function buildTeam(info) {
 
         // grab the keys from the data object (the names)
-        const people = Object.keys(info);
+        const team = Object.keys(info);
 
-        people.forEach(person => {
+        team.forEach(person => {
             let panel = theTemplate.cloneNode(true); // make a copy of the template content
             let containers = panel.firstElementChild.children; // get a reference to the template content
 
@@ -35,10 +38,12 @@
             containers[2].textContent = info[person].role;
             containers[3].textContent = info[person].nickname;
 
+            // put the virtual panel in the team selection in our HTML
             theTeam.appendChild(panel);
         });
 
     }
 
+    //invoke the getData function
     getData();
 })();

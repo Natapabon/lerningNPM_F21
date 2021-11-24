f044f7a9e3546e7c9eb6890a5ff630fc3c3aa886
Nata Pabon <ta@MacBook-Air-de-Ta.local>
2021-11-24 14:31:02 -0500

    adding some extra notes in index.php

--- includes/index.php
+++ includes/index.php
@@ -4,13 +4,17 @@
     require("functions.php");
 
     //filter the incoming basic request and call the appropiate function
-
+    //(these live in functions.php)
     if (isset($_GET["id"])) {
+        //This is the id that the user is passing to the script (1,2,3 etc) => ?id=1
+        //it referes to the row of data that we want to retrive from the DB
         $id = $_GET["id"];
-        
+
+        //create a variable to hold the result of the DB request
         $result = getOneProf($pdo, $id);
     } else {
         $result = getAllProfs($pdo);
     }
 
+    //send the DB result (our data) back to the javascript file
     echo json_encode($result);
